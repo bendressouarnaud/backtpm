22f2adeb06a0029efd02659d6b1328777689854d
ngbandamakonan <97701973+bendressouarnaud@users.noreply.github.com>
2023-05-29 21:55:35 +0200

    29052023

--- src/main/java/com/example/postgresql/demopostgre/DemopostgreApplication.java
+++ src/main/java/com/example/postgresql/demopostgre/DemopostgreApplication.java
@@ -2,9 +2,10 @@ package com.example.postgresql.demopostgre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemopostgreApplication {
+public class DemopostgreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemopostgreApplication.class, args);
